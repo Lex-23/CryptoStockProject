63c4d7ed89784d7d69ad77869b0391bafd315a18
aliaksei <liverpoolsmile23@gmail.com>
2021-11-05 15:10:06 +0300

    fix data in tests

--- cryptostock/account/tests/test_account_api_view.py
+++ cryptostock/account/tests/test_account_api_view.py
@@ -3,7 +3,7 @@
 
 def test_get_data_self_account(first_user_account, auth_first_user):
     response = auth_first_user.get("/api/account/")
-    actual_data = {
+    assert response.json() == {
         "id": first_user_account.id,
         "name": first_user_account.name,
         "owner": first_user_account.owner.username,
@@ -14,15 +14,13 @@ def test_get_data_self_account(first_user_account, auth_first_user):
         },
         "wallet_records": [],
     }
-    assert actual_data == response.json()
-    assert 0 == first_user_account.cash_balance
 
 
 def test_user_not_get_another_account(
     first_user_account, second_user_account, auth_second_user
 ):
     response = auth_second_user.get("/api/account/")
-    actual_data = {
+    assert response.json() == {
         "id": second_user_account.id,
         "name": second_user_account.name,
         "owner": second_user_account.owner.username,
@@ -33,4 +31,3 @@ def test_user_not_get_another_account(
         },
         "wallet_records": [],
     }
-    assert actual_data == response.json()
