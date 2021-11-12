c96e41b607c74c64a87790162f201f5baa0fcc46
Aliaksei Valenta <liverpoolsmile23@gmail.com>
2021-11-12 18:33:18 +0300

    add catch exc to delete test

--- cryptostock/account/tests/test_delete_salesdashboard.py
+++ cryptostock/account/tests/test_delete_salesdashboard.py
@@ -1,15 +1,20 @@
+import pytest
+from account.models import SalesDashboard
 from account.tests.factory import BrokerFactory, SalesDashboardFactory
+from django.http.response import Http404
+from rest_framework.generics import get_object_or_404
 
 
 def test_delete_sales_dashboard(auth_broker, broker_account):
     sale = SalesDashboardFactory(broker=broker_account)
 
     response = auth_broker.delete(f"/api/salesdashboard/{sale.id}/")
-    response1 = auth_broker.get(f"/api/salesdashboard/{sale.id}/")
+    with pytest.raises(Http404) as exc:
+        get_object_or_404(SalesDashboard.objects.all(), id=sale.id)
 
     assert response.status_code == 204
-    assert response1.status_code == 404
-    assert response1.json() == {"detail": "Not found."}
+    assert exc.type == Http404
+    assert "No SalesDashboard matches the given query." in str(exc.value)
 
 
 def test_delete_sales_dashboard_not_broker(auth_client, broker_account):
