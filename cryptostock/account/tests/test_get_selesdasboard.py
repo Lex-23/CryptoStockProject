ae1b8da1d28138a5cf85b54a0053ee23f33083fb
Lex-23 <Liverpoolsmile23@gmail.com>
2021-11-12 14:03:10 +0300

    Feature/test get sale (#9)
    * create tests for get salesdasboard unit

---
+++ cryptostock/account/tests/test_get_selesdasboard.py
@@ -0,0 +1,32 @@
+from account.tests.factory import SalesDashboardFactory
+
+
+def test_get_sales_dashboard(auth_user, broker_account):
+    sale = SalesDashboardFactory(broker=broker_account)
+
+    response = auth_user.get(f"/api/salesdashboard/{sale.id}/")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": sale.id,
+        "asset": {
+            "id": sale.asset.id,
+            "name": sale.asset.name,
+            "description": sale.asset.description,
+        },
+        "count": sale.count,
+        "price": sale.price,
+        "broker": {
+            "id": sale.broker.id,
+            "name": sale.broker.name,
+            "owner": sale.broker.owner.username,
+            "wallet": {"id": sale.broker.wallet.id, "name": sale.broker.wallet.name},
+        },
+    }
+
+
+def test_get_salesdashboard_not_authenticated_user(api_client, broker_account):
+    sale = SalesDashboardFactory(broker=broker_account)
+
+    response = api_client.get(f"/api/salesdashboard/{sale.id}/")
+    assert response.status_code == 401
