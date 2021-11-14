e917e5c38c00c3ddf19bae032d3502b493ab92cc
Aliaksei Valenta <liverpoolsmile23@gmail.com>
2021-11-14 17:49:58 +0300

    fix deleting test

--- cryptostock/account/tests/test_delete_salesdashboard.py
+++ cryptostock/account/tests/test_delete_salesdashboard.py
@@ -1,20 +1,16 @@
 import pytest
 from account.models import SalesDashboard
 from account.tests.factory import BrokerFactory, SalesDashboardFactory
-from django.http.response import Http404
-from rest_framework.generics import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def test_delete_sales_dashboard(auth_broker, broker_account):
     sale = SalesDashboardFactory(broker=broker_account)
 
     response = auth_broker.delete(f"/api/salesdashboard/{sale.id}/")
-    with pytest.raises(Http404) as exc:
-        get_object_or_404(SalesDashboard.objects.all(), id=sale.id)
-
+    with pytest.raises(ObjectDoesNotExist, match=r".* does not exist."):
+        SalesDashboard.objects.get(id=sale.id)
     assert response.status_code == 204
-    assert exc.type == Http404
-    assert "No SalesDashboard matches the given query." in str(exc.value)
 
 
 def test_delete_sales_dashboard_not_broker(auth_client, broker_account):
