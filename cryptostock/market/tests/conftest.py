f673ea84ecd4f91686af565fc90997d49fd5b095
aliaksei <liverpoolsmile23@gmail.com>
2021-12-07 16:54:41 +0300

    add all tests for get_assets endpoint

--- cryptostock/market/tests/conftest.py
+++ cryptostock/market/tests/conftest.py
@@ -1,8 +1,4 @@
-from account.tests import conftest as account_conftest
-
-user_one = account_conftest.user_one
-api_client = account_conftest.api_client
-auth_factory = account_conftest.auth_factory
-auth_broker = account_conftest.auth_broker
-account_factory = account_conftest.account_factory
-broker_account = account_conftest.broker_account
+try:
+    from account.tests.conftest import *  # noqa
+except ImportError:
+    pass
--- cryptostock/market/tests/test_get_assets.py
+++ cryptostock/market/tests/test_get_assets.py
@@ -1,15 +1,34 @@
 from unittest.mock import MagicMock
 
-from market.models import Market, YahooMarket
-from market.tests.factory import assets_list
+from market.models import YahooMarket
+from market.tests.factory import MarketFactory, assets_list
 
 
 def test_get_assets_from_market_name(auth_broker):
     name = "Yahoo"
     YahooMarket.get_assets = MagicMock(return_value=assets_list)
-    Market.objects.create(name=name)
-
+    MarketFactory(name=name)
     response = auth_broker.get(f"/api/market/{name}/asset/")
 
     assert response.status_code == 200
     assert response.json() == assets_list
+
+
+def test_get_assets_from_market_name_not_broker(auth_client):
+    name = "Yahoo"
+    MarketFactory(name=name)
+
+    response = auth_client.get(f"/api/market/{name}/asset/")
+
+    assert response.status_code == 400
+    assert response.json() == [
+        "You are not a broker. You haven`t permissions for this operation."
+    ]
+
+
+def test_get_assets_from_market_not_authenticated_user(api_client):
+    name = "Yahoo"
+
+    response = api_client.get(f"/api/market/{name}/asset/")
+
+    assert response.status_code == 401
