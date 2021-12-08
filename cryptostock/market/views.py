f2e8e9bc4daa8352d8ee0c56ac5bdd1fb6ae1ff8
aliaksei <liverpoolsmile23@gmail.com>
2021-12-08 19:17:01 +0300

    create view for buy asset

--- cryptostock/market/views.py
+++ cryptostock/market/views.py
@@ -1,9 +1,14 @@
 from market.models import Market
+from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from utils.validators import validate_is_broker
+
+
+class AssetBuySerializer(serializers.ModelSerializer):
+    count = serializers.IntegerField()
 
 
 class AssetMarketListApiView(APIView):
@@ -23,3 +28,12 @@ class AssetMarketApiView(APIView):
                 [f"asset {asset_name} not allow for market {market_name}."]
             )
         return Response(asset)
+
+
+class BuyAssetMarketApiView(APIView):
+    def post(self, request, market_name, asset_name, format=None):
+        validate_is_broker(request)
+        count = request.data["count"]
+        AssetBuySerializer(data=count).is_valid(raise_exception=True)
+        market = get_object_or_404(queryset=Market.objects.all(), name=market_name)
+        return Response(market.client.buy(name=asset_name, count=count))
