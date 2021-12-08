f210ea8f6eb60cae7737c7d370604261b013e209
aliaksei <liverpoolsmile23@gmail.com>
2021-12-08 11:01:05 +0300

    add test db_calls

--- cryptostock/market/tests/test_get_assets.py
+++ cryptostock/market/tests/test_get_assets.py
@@ -1,5 +1,7 @@
 from unittest.mock import MagicMock
 
+from django.db import connection
+from django.test.utils import CaptureQueriesContext
 from market.models import YahooMarket
 from market.tests.factory import MarketFactory, assets_list
 
@@ -32,3 +34,15 @@ def test_get_assets_from_market_not_authenticated_user(api_client):
     response = api_client.get(f"/api/market/{name}/asset/")
 
     assert response.status_code == 401
+
+
+def test_get_assets_from_market_name_db_calls(auth_broker):
+    name = "Yahoo"
+    YahooMarket.get_assets = MagicMock(return_value=assets_list)
+    MarketFactory(name=name)
+
+    with CaptureQueriesContext(connection) as query_context:
+        response = auth_broker.get(f"/api/market/{name}/asset/")
+
+    assert response.status_code == 200
+    assert len(query_context) == 2
