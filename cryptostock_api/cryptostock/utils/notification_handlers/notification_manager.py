826ff8382f82f9dd58f38ca24bcbf87921d4423a
Aliaksei Valenta <liverpoolsmile23@gmail.com>
2021-12-31 13:14:23 +0300

    create func notify

--- cryptostock_api/cryptostock/utils/notification_handlers/notification_manager.py
+++ cryptostock_api/cryptostock/utils/notification_handlers/notification_manager.py
@@ -1,6 +1,8 @@
 from account.models import Account
 from notification.models import Notifier
 
+from cryptostock_api.cryptostock.notification.models import Consumer, TemplaterRegister
+
 
 def notification_manager(account_id, **kwargs):
     active_account_notifiers = Notifier.objects.filter(
@@ -8,3 +10,15 @@ def notification_manager(account_id, **kwargs):
     )
     for notifier in active_account_notifiers:
         notifier.type.send_notification(**kwargs)
+
+
+def notify(notification_type, account_id, **data):
+    account = Account.objects.get(id=account_id)
+    enabled_consumers = Consumer.objects.get(account=account).enable_consumers
+    if not enabled_consumers:
+        pass
+
+    for consumer in account.enabled_consumers:
+        templater = TemplaterRegister.get(consumer.type, notification_type)
+        message = templater.render(data)
+        consumer.send(message, **data)
