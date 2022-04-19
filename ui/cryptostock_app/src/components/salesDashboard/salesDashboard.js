c526f394666fedcbf2be1a6df392d3a678ac60e2
Aliaksei Valenta <liverpoolsmile23@gmail.com>
2022-04-19 16:09:09 +0400

    add pagination, catching error and minimal css style

--- ui/cryptostock_app/src/components/salesDashboard/salesDashboard.js
+++ ui/cryptostock_app/src/components/salesDashboard/salesDashboard.js
@@ -1,14 +1,14 @@
 import React from "react";
+import "./salesDashboard.css";
 
 function SaleItem(props) {
   return (
-    <div className="container">
+    <div className="sale-container">
       <div className="sale__asset-name">{props.sale.asset.name}</div>
       <div className="sale__asset-desc">{props.sale.asset.description}</div>
-      <div className="sale__count">{props.sale.count}</div>
-      <div className="sale__price">{props.sale.price}</div>
-      <div className="sale__broker">{props.sale.broker.name}</div>
-      <br /> <hr />
+      <div className="sale__count">Count: {props.sale.count}</div>
+      <div className="sale__price">Price: {props.sale.price}</div>
+      <div className="sale__broker">Saler: {props.sale.broker.name}</div>
     </div>
   );
 }
--- ui/cryptostock_app/src/pages/salesDashboardList/index.js
+++ ui/cryptostock_app/src/pages/salesDashboardList/index.js
@@ -1,30 +1,60 @@
 import React, { Component } from "react";
 import axiosInstance from "../../axiosApi";
 import SaleItem from "../../components/salesDashboard/salesDashboard";
+import ReactPaginate from "react-paginate";
+import "./Pagination.css";
 
 class SalesDashboardList extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            error: null,
+            isLoaded: false,
+            offset: 0,
             salesList: [],
+            perPage: 5,
+            currentPage: 0,
         };
-
+        this.handlePageClick = this.handlePageClick.bind(this);
         this.getSalesList = this.getSalesList.bind(this)
     }
 
     async getSalesList(){
         try {
-            let response = await axiosInstance.get('/salesdashboard/');
+            let response = await axiosInstance.get('/salesdashboard/', {
+                params: {
+                    limit: this.state.perPage,
+                    offset: this.state.offset,
+                }
+            });
             const salesList = response.data;
             this.setState({
+                pageCount: Math.ceil(response.data.count / this.state.perPage),
                 salesList: salesList.results,
+                isLoaded: true,
             });
             return salesList;
         } catch(error) {
             console.log("Error: ", JSON.stringify(error, null, 4));
+            this.setState({
+                isLoaded: true,
+                error,
+            });
             throw error;
         }
     }
+
+    handlePageClick(e) {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
+        this.setState(
+          {
+            currentPage: selectedPage,
+            offset: offset,
+          },
+          () => this.getSalesList()
+        );
+      }
 
     componentDidMount(){
         const salesData = this.getSalesList();
@@ -32,13 +62,27 @@ class SalesDashboardList extends Component {
     }
 
     render() {
-
-        //else if (!isLoaded) {
-        //    return <div>Loading...</div>;
-        //}
-
+        const { error, isLoaded } = this.state;
+        if (error) {
+          return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+          return <div>Loading...</div>;
+        } else {
             return (
                 <div>
+                    <ReactPaginate
+                        previousLabel={"previous"}
+                        nextLabel={"next"}
+                        breakLabel={"..."}
+                        breakClassName={"break-me"}
+                        pageCount={this.state.pageCount}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={1}
+                        onPageChange={this.handlePageClick}
+                        containerClassName={"pagination"}
+                        subContainerClassName={"pages pagination"}
+                        activeClassName={"active"}
+                    />
                     <div className="sale">
                         <div className="container">
                             {this.state.salesList.map((sale) => (
@@ -48,7 +92,7 @@ class SalesDashboardList extends Component {
                     </div>
                 </div>
             );
-
+        }
     }
 }
 
