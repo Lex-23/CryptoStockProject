302fa57c5241a66b444e1c4503ef08da2d636931
Aliaksei Valenta <liverpoolsmile23@gmail.com>
2022-04-20 21:33:58 +0400

    move nginx config for docker to ui dir, update Dockerfile dor ui, update settings.py for static

--- cryptostock_api/cryptostock/cryptostock/settings.py
+++ cryptostock_api/cryptostock/cryptostock/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "/django_static/"
+STATICFILES_DIRS = [BASE_DIR / "django_static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
