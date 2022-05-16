8d6130105d5e07abfb0a4d92a19a0d9559c4ce9a
Aliaksei Valenta <liverpoolsmile23@gmail.com>
2022-05-16 13:23:24 +0400

    fix Makefile (add STAGE for be build), update run.sh, be-django/deployment (APP_PORT), be Dockerfile (python version)

--- cryptostock_api/cryptostock/run.sh
+++ cryptostock_api/cryptostock/run.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 python manage.py createsuperuser --no-input
 python manage.py collectstatic --no-input
-gunicorn cryptostock.wsgi:application --bind 0.0.0.0:8543 --log-level info --timeout 180  --workers 3
+gunicorn cryptostock.wsgi:application --bind 0.0.0.0:${APP_PORT} --log-level info --timeout 180 --workers 3
